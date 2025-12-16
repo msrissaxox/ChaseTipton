bd9da49f114483f9bccc485571e1d84c24307e72
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-12-16 16:49:58 -0500

    add flex styling to nav bar

--- my-nextjs-app/components/Nav.js
+++ my-nextjs-app/components/Nav.js
@@ -5,20 +5,20 @@ export default function Nav(){
     return(
         <div className="fixed top-0 w-full z-50">
             <Divider sx={{ backgroundColor: '#bfbab0' }} />
-            <nav className="w-full py-4 px-8 dark:bg-gray-900 shadow-md" style={{ backgroundColor: '#1d1c1c' }}>
+            <nav className="flex justify-between items-center w-full py-4 px-8 dark:bg-gray-900 shadow-md" style={{ backgroundColor: '#1d1c1c' }}>
+                <div>
+                    <a href="/" className="dark:text-gray-200 hover:underline">
+                        CHASE TIPTON
+                    </a>
+                </div>
                 <ul className="flex space-x-6">
                     <li>
-                        <a href="/" className="dark:text-gray-200 hover:underline">
-
-CHASE TIPTON                    </a>
-                    </li>
-                    <li>
-                        <a href="/about" className=" dark:text-gray-200 hover:underline">
+                        <a href="/about" className="dark:text-gray-200 hover:underline">
                             MORE
                         </a>
                     </li>
                     <li>
-                        <a href="/contact" className=" dark:text-gray-200 hover:underline">
+                        <a href="/contact" className="dark:text-gray-200 hover:underline">
                             CONTACT
                         </a>
                     </li>
