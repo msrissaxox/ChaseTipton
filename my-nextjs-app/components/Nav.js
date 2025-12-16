8bf12d0a5a7ca2b65c0f76fffcf3e420f85af1ec
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-12-16 16:44:57 -0500

    added styling to nav

--- my-nextjs-app/components/Nav.js
+++ my-nextjs-app/components/Nav.js
@@ -1,26 +1,30 @@
 import React from "react";
+import Divider from '@mui/material/Divider';
 
 export default function Nav(){
     return(
-        <nav className="w-full py-4 px-8 bg-white dark:bg-gray-900 shadow-md">
-            <ul className="flex space-x-6">
-                <li>
-                    <a href="/" className="text-gray-800 dark:text-gray-200 hover:underline">
+        <div className="fixed top-0 w-full z-50">
+            <Divider sx={{ backgroundColor: '#bfbab0' }} />
+            <nav className="w-full py-4 px-8 dark:bg-gray-900 shadow-md" style={{ backgroundColor: '#1d1c1c' }}>
+                <ul className="flex space-x-6">
+                    <li>
+                        <a href="/" className="dark:text-gray-200 hover:underline">
 
-                        Home
-                    </a>
-                </li>
-                <li>
-                    <a href="/about" className="text-gray-800 dark:text-gray-200 hover:underline">
-                        About
-                    </a>
-                </li>
-                <li>
-                    <a href="/contact" className="text-gray-800 dark:text-gray-200 hover:underline">
-                        Contact
-                    </a>
-                </li>
-            </ul>
-        </nav>
+CHASE TIPTON                    </a>
+                    </li>
+                    <li>
+                        <a href="/about" className=" dark:text-gray-200 hover:underline">
+                            MORE
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/contact" className=" dark:text-gray-200 hover:underline">
+                            CONTACT
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <Divider sx={{ backgroundColor: '#bfbab0' }} />
+        </div>
     )
 }
