fd282d125c69485856b773bf5a1a58c1c0f17138
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-12-22 11:09:42 -0500

    added sitemap

---
+++ my-nextjs-app/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://chasetakephoto.com',
+      lastModified: '2025-12-22T16:02:49+00:00',
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ]
+}
